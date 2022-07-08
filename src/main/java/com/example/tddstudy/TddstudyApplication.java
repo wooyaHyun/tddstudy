1df49da2980ac7aa983c55e02d54be2fb95ae523
wooyaHyun <tlagusdn34@nate.com>
2022-07-08 17:26:44 +0900

    Initial commit

---
+++ src/main/java/com/example/tddstudy/TddstudyApplication.java
@@ -0,0 +1,11 @@
+package com.example.tddstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddstudyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TddstudyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/tddstudy/TddstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
